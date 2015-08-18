58d0155d85ab4767c13b7e42162e5d39f490938d
Jari <jtorniainen@users.noreply.github.com>
2015-08-18 13:32:33 +0300

    changed lsl import to external module

--- mwl_example/stream_mwl_data.py
+++ mwl_example/stream_mwl_data.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
 
-from midas import pylsl_python3 as lsl
 import time
+import pylsl as lsl
+
 
 def read_data(fname):
     data = open(fname, "r").readlines()
     data = [float(i.strip()) for i in data]
     return data
 
+
 def equalize(x, n):
     return x[:(n-1)]
 
-## read the data
+# read the data
 d_ecg = read_data("data/data_ecg.csv")
-d_fz  = read_data("data/data_eeg_fz.csv")
-d_pz  = read_data("data/data_eeg_pz.csv")
+d_fz = read_data("data/data_eeg_fz.csv")
+d_pz = read_data("data/data_eeg_pz.csv")
 
-## make the data equally long
-dset              = (d_ecg, d_fz, d_pz)
-n                 = min(list(map(len, dset)))
+# make the data equally long
+dset = (d_ecg, d_fz, d_pz)
+n = min(list(map(len, dset)))
 d_ecg, d_fz, d_pz = [equalize(d, n) for d in dset]
 
-## create outlets
-info_ecg   = lsl.StreamInfo('n1_ecg','ECG',1,100,'float32','ecg123');
-info_eeg   = lsl.StreamInfo('n1_eeg','EEG',2,100,'float32','ecg123');
+# create outlets
+info_ecg = lsl.StreamInfo('n1_ecg', 'ECG', 1, 100, 'float32', 'ecg123')
+info_eeg = lsl.StreamInfo('n1_eeg', 'EEG', 2, 100, 'float32', 'ecg123')
 outlet_ecg = lsl.StreamOutlet(info_ecg)
 outlet_eeg = lsl.StreamOutlet(info_eeg)
 
-## stream the data
+# stream the data
 print("streaming data")
 for i in range(n):
     outlet_ecg.push_sample([d_ecg[i]])
